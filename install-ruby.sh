bf455f6ab72418424b023dc9c7432bd9709710af
ocha <ochaochaocha3@gmail.com>
2017-03-21 15:54:50 +0900

    Rubyインストールのオプションに--disable-install-rdocを追加する

--- install-ruby.sh
+++ install-ruby.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-MAKEOPTS='-j4' RUBY_CONFIGURE_OPTS='--enable-shared' rbenv install --verbose $1
+MAKEOPTS='-j4' RUBY_CONFIGURE_OPTS='--enable-shared --disable-install-rdoc' rbenv install --verbose $1
